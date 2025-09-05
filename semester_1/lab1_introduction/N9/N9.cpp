13225362a1171276dc6246e9a1b7eda5b6b428ef
Andrei <andreipalevoda@gmail.com>
2025-09-05 21:53:41 +0300

    Сделаны пункты 9 и 13

---
+++ semester_1/lab1_introduction/N9/N9.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int number = 0;
+    std::cout << "Введите число для проверки на то, является ли оно палиндромом: ";
+    std::cin >> number;
+    int n = 3;
+    int num[4];
+    int a = 0;
+    while (n >= 0) {
+        a = number % 10;
+        num[n] = a;
+        number = number / 10;
+        n = n - 1;
+    }
+    
+    if (num[0] == num[3] && num[2] == num[1]) {
+        std::cout << "Чичло является палиндромом";
+    }
+    else {
+        std::cout << "Чичло не является палиндромом";
+    }
+
+    return 0;
+}

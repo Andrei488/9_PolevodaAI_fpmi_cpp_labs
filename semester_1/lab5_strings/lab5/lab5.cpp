327e4448d78821d21063b242c137addb8d7efb11
Andrei488 <andreipalevoda@gmail.com>
2025-10-01 12:14:28 +0300

    test

--- semester_1/lab5_strings/lab5/lab5.cpp
+++ semester_1/lab5_strings/lab5/lab5.cpp
@@ -1,8 +1,16 @@
 ﻿// lab5.cpp
  
+#include <string>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    std::string text;
+    std::cout << "Enter text: ";
+    std::getline(std::cin, text);
+
+    size_t pos = text.find("welcome");
+    if (pos != std::string::npos) {
+        std::cout << "Find on id: " << pos << std::endl;
+    }
 }

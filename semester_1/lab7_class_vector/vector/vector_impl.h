4e41e5d451d2a7260abab814625452cffd0b4d56
Andrei488 <andreipalevoda@gmail.com>
2025-11-26 21:53:01 +0300

    Не работают тесты

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,39 @@
 #pragma once
+#include <initializer_list>
+#include <stdexcept>
+#include <ostream>
 
 class Vector {
-    // your code goes here
+private:
+    int* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 20;
+
+    void reallocate(size_t new_capacity);
+
+public:
+    Vector();
+    Vector(size_t size);
+    Vector(std::initializer_list<int> list);
+    Vector(const Vector& other);
+    Vector& operator=(const Vector& other);
+    void swap(Vector& other) noexcept;
+
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+
+    int& at(size_t index);
+    const int& at(size_t index) const;
+
+    size_t size() const;
+    size_t capacity() const;
+
+    void push_back(int value);
+    void pop_back();
+    void clear();
+    void reserve(size_t new_capacity);
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector& vec);
+
+    ~Vector();
 };

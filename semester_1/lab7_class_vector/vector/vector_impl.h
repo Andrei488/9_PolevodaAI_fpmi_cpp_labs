9b6221a76b6926d750d7c789e898eb82adf8c04e
Andrei488 <andreipalevoda@gmail.com>
2025-12-07 22:51:48 +0300

    Победа номер 7

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,6 +1,6 @@
 #pragma once
+#include <cstddef>
 #include <initializer_list>
-#include <stdexcept>
 #include <ostream>
 
 class Vector {
@@ -8,139 +8,29 @@ private:
     int* data_ = nullptr;
     size_t size_ = 0;
     size_t capacity_ = 0;
-
-    void reallocate(size_t new_capacity) {
-        int* new_data = new int[new_capacity];
-        for (size_t i = 0; i < size_; i++) {
-            new_data[i] = data_[i];
-        }
-        delete[] data_;
-        data_ = new_data;
-        capacity_ = new_capacity;
-    }
+    void reallocate(size_t new_capacity);
 
 public:
     Vector() = default;
+    explicit Vector(size_t size);
+    Vector(std::initializer_list<int> list);
+    Vector(const Vector& other);
+    Vector& operator=(const Vector& other);
+    ~Vector();
 
-    Vector(size_t size) : size_(size), capacity_(size > 0 ? size : 0) {
-        if (capacity_ > 0) {
-            data_ = new int[capacity_];
-            for (size_t i = 0; i < size_; i++) {
-                data_[i] = 0;
-            }
-        }
-    }
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+    int& At(size_t index);
+    const int& At(size_t index) const;
 
-    Vector(std::initializer_list<int> list) : size_(list.size()), capacity_(list.size()) {
-        if (capacity_ > 0) {
-            data_ = new int[capacity_];
-            size_t i = 0;
-            for (int value : list) {
-                data_[i++] = value;
-            }
-        }
-    }
+    size_t Size() const;
+    size_t Capacity() const;
 
-    Vector(const Vector& other) : size_(other.size_), capacity_(other.capacity_) {
-        if (capacity_ > 0) {
-            data_ = new int[capacity_];
-            for (size_t i = 0; i < size_; i++) {
-                data_[i] = other.data_[i];
-            }
-        }
-    }
+    void PushBack(int value);
+    void PopBack();
+    void Clear();
+    void Reserve(size_t new_capacity);
+    void Swap(Vector& other) noexcept;
 
-    Vector& operator=(const Vector& other) {
-        if (this != &other) {
-            delete[] data_;
-            size_ = other.size_;
-            capacity_ = other.capacity_;
-
-            if (capacity_ > 0) {
-                data_ = new int[capacity_];
-                for (size_t i = 0; i < size_; i++) {
-                    data_[i] = other.data_[i];
-                }
-            } else {
-                data_ = nullptr;
-            }
-        }
-        return *this;
-    }
-
-    void Swap(Vector& other) noexcept {
-        std::swap(data_, other.data_);
-        std::swap(size_, other.size_);
-        std::swap(capacity_, other.capacity_);
-    }
-
-    int& operator[](size_t index) {
-        return data_[index];
-    }
-
-    const int& operator[](size_t index) const {
-        return data_[index];
-    }
-
-    int& At(size_t index) {
-        if (index >= size_) {
-            throw std::out_of_range("Index out of range");
-        }
-        return data_[index];
-    }
-
-    const int& At(size_t index) const {
-        if (index >= size_) {
-            throw std::out_of_range("Index out of range");
-        }
-        return data_[index];
-    }
-
-    size_t Size() const {
-        return size_;
-    }
-
-    size_t Capacity() const {
-        return capacity_;
-    }
-
-    void PushBack(int value) {
-        if (size_ >= capacity_) {
-            size_t new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
-            reallocate(new_capacity);
-        }
-        data_[size_++] = value;
-    }
-
-    void PopBack() {
-        if (size_ > 0) {
-            size_--;
-        }
-    }
-
-    void Clear() {
-        size_ = 0;
-    }
-
-    void Reserve(size_t new_capacity) {
-        if (new_capacity > capacity_) {
-            reallocate(new_capacity);
-        }
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Vector& vec) {
-        os << "[";
-        for (size_t i = 0; i < vec.size_; i++) {
-            os << vec.data_[i];
-            if (i != vec.size_ - 1) {
-                os << ", ";
-            }
-        }
-        os << "]";
-        return os;
-    }
-
-    ~Vector() {
-        delete[] data_;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Vector& vec);
 };
